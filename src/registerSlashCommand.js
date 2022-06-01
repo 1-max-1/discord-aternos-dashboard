733d2c0385ef115b8f2dda2a6cbe8307982b2935
1-max-1 <maxh2815@gmail.com>
2022-06-01 16:01:49 +1200

    Removed slash command permission update Discord has made some changes to their permission system and bots are no longer allowed to update permissions for slash commands. Will just check permissions manually in code.

--- src/registerSlashCommand.js
+++ src/registerSlashCommand.js
@@ -3,28 +3,14 @@ const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const config = require("../config.json");
 
-const rest = new REST({ version: "9" }).setToken(config.TOKEN);
+const rest = new REST({ "version": "9" }).setToken(config.TOKEN);
 
 const commands = [
 	new SlashCommandBuilder().setName("sendmessage").setDescription("Sends the server control message to the current channel.").setDefaultPermission(false).toJSON()
 ];
 
-const commandPermissions = {
-	"permissions": [{
-		"id": config.DEV_ID,
-		"type": 2, // 2 is USER, 1 is ROLE
-		"permission": true
-	}]
-};
-
 async function main() {
 	// Register the slash command
 	await rest.put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), {"body": commands});
-
-	// Get the ID of the slash command
-	let commandData = await rest.get(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID));
-
-	// Update slash command so only I can use it
-	await rest.put(Routes.applicationCommandPermissions(config.CLIENT_ID, config.GUILD_ID, commandData[0]["id"]), {"body": commandPermissions});
 }
 main();
